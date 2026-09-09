e74b589d07fb4f4151177088857a588f582b6993
SageBrian <briankt48@gmail.com>
2026-09-09 17:18:03 +0300

    connected task to postgress

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from database import Base, engine
+
+from database import Base, engine, SessionLocal
 from models import Task
 
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="DevOps CI/CD API",
     version="1.0.0"
 )
 
-Base.metadata.create_all(bind=engine)
 
 class TaskCreate(BaseModel):
     title: str
@@ -17,8 +20,12 @@ class TaskCreate(BaseModel):
     completed: bool = False
 
 
-tasks = {}
-next_id = 1
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -40,42 +47,20 @@ def version():
 
 
 @app.post("/tasks", status_code=201)
-def create_task(task: TaskCreate):
-    global next_id
+def create_task(task: TaskCreate, db: Session = Depends(get_db)):
+    new_task = Task(
+        title=task.title,
+        description=task.description,
+        completed=task.completed
+    )
 
-    task_data = task.model_dump()
-    task_data["id"] = next_id
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
 
-    tasks[next_id] = task_data
-    next_id += 1
-
-    return task_data
+    return new_task
 
 
 @app.get("/tasks")
-def list_tasks():
-    return list(tasks.values())
-
-
-@app.get("/tasks/{task_id}")
-def get_task(task_id: int):
-    if task_id not in tasks:
-        raise HTTPException(
-            status_code=404,
-            detail="Task not found"
-        )
-
-    return tasks[task_id]
-
-
-@app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    if task_id not in tasks:
-        raise HTTPException(
-            status_code=404,
-            detail="Task not found"
-        )
-
-    del tasks[task_id]
-
-    return {"message": "Task deleted"}
+def list_tasks(db: Session = Depends(get_db)):
+    return db.query(Task).all()
